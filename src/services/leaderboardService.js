512e07c978584b1c9de2fcba987b507c0ab7bcc8
baticlean <baticlean225@gmail.com>
2026-04-19 21:47:53 +0000

    Top10

--- src/services/leaderboardService.js
+++ src/services/leaderboardService.js
@@ -2,11 +2,10 @@
 const User = require('../models/User');
 
 /**
- * Récupère les meilleurs joueurs selon leur score record
- * @param {number} limit - Nombre maximum de joueurs à retourner
- * @returns {Promise<Array>} - Liste des joueurs
+ * Récupère les meilleurs joueurs pour le Top 3 + Top 10 de la liste
+ * @param {number} limit - On passe à 13 pour avoir un compte rond dans la liste
  */
-exports.fetchGlobalTopPlayers = async (limit = 10) => {
+exports.fetchGlobalTopPlayers = async (limit = 13) => {
     return await User.find({ isBanned: false })
         .select('login bestScore xp level')
         .sort({ bestScore: -1 })
