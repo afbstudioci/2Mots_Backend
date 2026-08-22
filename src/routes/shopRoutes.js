32c64823f8ca74a1fe9076c51e239fbf1b1555ce
baticlean <baticlean225@gmail.com>
2026-08-22 12:46:03 +0000

    suppresion de compte 2

--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -1,4 +1,4 @@
-﻿//src/routes/shopRoutes.js
+//src/routes/shopRoutes.js
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middlewares/auth');
@@ -8,6 +8,5 @@ router.get('/catalog', protect, shopController.getCatalog);
 router.post('/buy-with-kevs', protect, shopController.buyWithKevs);
 router.post('/verify-purchase', protect, shopController.verifyPurchase);
 router.post('/use-booster', protect, shopController.useBooster);
-router.post('/equip-cosmetic', protect, shopController.equipCosmetic);
 
 module.exports = router;
